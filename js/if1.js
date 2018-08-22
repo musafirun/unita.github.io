9e9a6d5442b31b0ef53456cbf2dda66fecd24824
money8beat <money00@mail.ru>
2018-08-22 15:11:27 +0500

    1

---
+++ js/if1.js
@@ -0,0 +1,28 @@
+
+
+
+$("document").ready(function(){
+ 
+ 	var dostup = prompt("Enter only for owner");
+ 		if (dostup == "admin") {
+ 		var pass = prompt("Password");
+ 			if (pass == "money00") {
+ 				alert( "Welcome Mr. Mansur");
+ 			}
+ 			else if (pass == false) {
+ 				alert("Operation disturbing");
+ 			}
+ 			else {
+ 				alert("Liar Bastard");
+ 			}
+ 		}
+ 		else if (false) {
+ 			alert('Enter disturbing');
+ 		}
+ 		else {
+ 			alert("Nope, Try again");
+ 			
+ 		}
+ 			
+ 		
+});
---
+++ js/vychislyaet po vozrastu god rojdeniya.js
@@ -0,0 +1,14 @@
+
+
+
+$("document").ready(function(){
+ 
+ 	var imy = prompt("Сколько вам лет?");
+		var tg = 2017
+		imy = parseInt(imy);
+		res = tg-imy	
+
+
+		alert("Вам " + imy + " лет" + '\n' + "Вы родились в " + res + " году");
+	
+});
